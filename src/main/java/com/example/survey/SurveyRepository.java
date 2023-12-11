888420302863b5c0359f99fd2c8a136c42c6b888
Omar Shabaan <omar.plus.077@gmail.com>
2023-12-11 16:10:16 +0200

    Retrieve survey by title, addressing N+1 problem.

---
+++ src/main/java/com/example/survey/SurveyRepository.java
@@ -0,0 +1,16 @@
+package com.example.survey;
+
+import com.example.survey.entity.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+
+  @Query("SELECT DISTINCT s FROM Survey s LEFT JOIN FETCH s.questions q LEFT JOIN FETCH q.choices c WHERE s.title = :surveyTitle")
+  Optional<Survey> findByTitle(@Param("surveyTitle") String surveyTitle);
+
+
+}
