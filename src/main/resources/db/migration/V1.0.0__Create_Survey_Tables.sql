0548e3f1b587c79652bbdd19d624f34d14585991
Omar Shabaan <omar.plus.077@gmail.com>
2023-12-13 15:49:03 +0200

    remove fk duplications and add NOT NULL constraints.

--- src/main/resources/db/migration/V1.0.0__Create_Survey_Tables.sql
+++ src/main/resources/db/migration/V1.0.0__Create_Survey_Tables.sql
@@ -14,9 +14,8 @@ CREATE TABLE survey
 CREATE TABLE survey_response
 (
     id         BIGINT PRIMARY KEY DEFAULT nextval('survey_response_id_seq'),
-    survey_id  BIGINT REFERENCES survey (id),
-    user_email VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_survey_response_survey FOREIGN KEY (survey_id) REFERENCES survey (id)
+    survey_id  BIGINT NOT NULL REFERENCES survey (id),
+    user_email VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE question
@@ -25,37 +24,31 @@ CREATE TABLE question
     text      VARCHAR(255) NOT NULL,
     type      VARCHAR(50)  NOT NULL,
     required  BOOLEAN      NOT NULL,
-    survey_id BIGINT REFERENCES survey (id),
-    CONSTRAINT fk_question_survey FOREIGN KEY (survey_id) REFERENCES survey (id)
+    survey_id BIGINT REFERENCES survey (id)
 );
 
 CREATE TABLE choice
 (
     id          BIGINT PRIMARY KEY DEFAULT nextval('choice_id_seq'),
     text        VARCHAR(255) NOT NULL,
-    question_id BIGINT REFERENCES question (id),
-    CONSTRAINT fk_choice_question FOREIGN KEY (question_id) REFERENCES question (id),
+    question_id BIGINT NOT NULL REFERENCES question (id),
     CONSTRAINT unique_choice_text UNIQUE (text)
 );
 
 CREATE TABLE user_answer
 (
     id                 BIGINT PRIMARY KEY DEFAULT nextval('user_answer_id_seq'),
-    survey_response_id BIGINT REFERENCES survey_response (id),
+    survey_response_id BIGINT NOT NULL REFERENCES survey_response (id),
     question_id        BIGINT REFERENCES question (id),
     text_answer        VARCHAR(255),
-    answered           BOOLEAN NOT NULL,
-    CONSTRAINT fk_user_answer_survey_response FOREIGN KEY (survey_response_id) REFERENCES survey_response (id),
-    CONSTRAINT fk_user_answer_question FOREIGN KEY (question_id) REFERENCES question (id)
+    answered           BOOLEAN NOT NULL
 );
 
 CREATE TABLE user_answer_choice
 (
     user_answer_id BIGINT REFERENCES user_answer (id),
     choice_id      BIGINT REFERENCES choice (id),
-    PRIMARY KEY (user_answer_id, choice_id),
-    CONSTRAINT fk_user_answer_choice_user_answer FOREIGN KEY (user_answer_id) REFERENCES user_answer (id),
-    CONSTRAINT fk_user_answer_choice_choice FOREIGN KEY (choice_id) REFERENCES choice (id)
+    PRIMARY KEY (user_answer_id, choice_id)
 );
 
 CREATE INDEX idx_survey_response_survey_id ON survey_response (survey_id);
